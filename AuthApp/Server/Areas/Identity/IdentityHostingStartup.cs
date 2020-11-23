6672fbd14b04cb89d3b70abe1e1c4ccb5b512ec0
Brian Parker <brian.parker@hotmail.com.au>
2020-11-24 01:10:19 +1100

    Saffold Identity pages (Login, Register & Manage)

---
+++ AuthApp/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AuthApp.Server.Data;
+using AuthApp.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthApp.Server.Areas.Identity.IdentityHostingStartup))]
+namespace AuthApp.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
