5b377a97a18fab204194612c3331f458bdc05e6f
Brian Parker <brian.parker@hotmail.com.au>
2020-11-24 01:07:46 +1100

    Application user properties extended Database migratation created

---
+++ AuthApp/Server/Data/Migrations/20201123140633_UpdateApplicationUserSchema.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthApp.Server.Data.Migrations
+{
+    public partial class UpdateApplicationUserSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DoB",
+                table: "AspNetUsers",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DoBVerified",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DoB",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DoBVerified",
+                table: "AspNetUsers");
+        }
+    }
+}
--- AuthApp/Server/Models/ApplicationUser.cs
+++ AuthApp/Server/Models/ApplicationUser.cs
@@ -1,7 +1,21 @@
 ﻿namespace AuthApp.Server.Models
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
     using Microsoft.AspNetCore.Identity;
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
+        [MaxLength(200)]
+        public string DisplayName { get; set; }
+
+        [PersonalData]
+        [Column("DoB")]
+        public DateTimeOffset? DateOfBirth { get; set; }
+
+        [PersonalData]
+        [Column("DoBVerified")]
+        public bool DateOfBirthVerified { get; set; }
     }
 }
