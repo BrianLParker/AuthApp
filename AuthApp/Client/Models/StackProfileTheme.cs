83791a3cd57eb7fd97e38f8220e4aa18adbc3750
Brian Parker <brian.parker@hotmail.com.au>
2020-11-26 12:55:11 +1100

    StackProfileLink

---
+++ AuthApp/Client/Models/StackProfileTheme.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Brian Parker. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace AuthApp.Client.Models
+{
+    public enum StackProfileTheme
+    {
+        Default,
+        Clean,
+        Dark,
+        Hotdog
+    }
+}
---
+++ AuthApp/Client/Views/Shared/StackProfileLink.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace AuthApp.Client.Views.Shared
+{
+    using AuthApp.Client.Models;
+    using Microsoft.AspNetCore.Components;
+
+    public partial class StackProfileLink
+    {
+        [Parameter]
+        public StackProfileTheme Theme { get; set; }
+
+        string query => Theme switch
+        {
+            StackProfileTheme.Clean => "?theme=clean",
+            StackProfileTheme.Dark => "?theme=dark",
+            StackProfileTheme.Hotdog => "?theme=hotdog",
+            _ => ""
+        };
+    }
+}
